413134f9f9f05bc071a6b8f12e5915bf3c547a2b
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:02:00 +0530

    Add files via upload
    multiline comments

---
+++ python/04.py
@@ -0,0 +1,45 @@
+# name = "pankaj"
+
+# print(type(name))
+# print(type(name) == str)
+# print(isinstance(name,str))  # kya name str hai 
+
+
+# pahle se constructor de sakte hai designed karke 
+
+name = str("pankaj")
+# print(type(name))
+# print(type(name) == str)
+# print(isinstance(name,str))  # kya name str hai 
+
+
+
+# multiple line comment ka tarika 
+
+muiltiple = """
+jklaslf
+;alfj
+a;sldjf
+af;dj;l
+a;lkdsjf;
+hello 
+hey
+chodo 
+chalo kaam ki baat 
+pe aate hai 
+jitna apki ladki ek mahine me udati hai utna to meri gadi tel khati hai 
+uncle namste """ 
+ # this is multi line comment
+
+# print(muiltiple)
+
+# boolean value 
+
+# a = 1 
+# print(type(a))
+
+
+# random no.
+
+import random;
+print(random.randrange(1,10))
