368c791401d864eacb9bf3775d7195f84c8259aa
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:06:29 +0530

    Add files via upload
    user input

---
+++ python/06.py
@@ -0,0 +1,9 @@
+# user se input
+
+name = input("What is your name ? ")
+# print(name)
+
+college = input("which college you studies")
+# print(college)
+name = "Hello " + input("What is your name ? ")
+print(name)
