39f4837f58d6c64296141489ead0faa918146546
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:07:39 +0530

    Add files via upload
    Tuple

---
+++ python/08.py
@@ -0,0 +1,82 @@
+#conditionals 
+# age = 17
+# if age >= 18:
+#     print("you are eligible ")
+
+# else:
+#      print("you are not eligible ")
+
+# else if 
+
+# a = 5
+# b=5
+# if(a>b):
+#     print("A is greater then B")
+# elif(b==a):
+#     print("A is equal to B")
+
+# else:
+#     print("B is greater than")
+
+# a = input("Enter A: ")
+# b = input("Enter B: ")
+
+# if a>b:
+#     print("A is greater then B")
+
+# elif b==a:
+#     print("A is equal to B")
+
+# else:
+#     print("B is greater than")
+
+# a = input("Enter A: ")  # Assuming user enters 19
+# b = input("Enter B: ")  # Assuming user enters 5      # Baad me dekhungaa
+
+# if (a > b):
+#     print("A is greater than B")
+# elif a == b:
+#     print("A is equal to B")
+# else:
+#     print("B is greater than A")
+
+
+
+
+# a =10
+# b = 1      # turnary operator jesaa hi hai 
+
+# print("A") if a>b else print ("B")
+
+
+# a = 20
+# b = 15
+# c = 10
+
+# if a>b and c>a:
+#     print("Both conditions are true")
+
+# else:
+#     print("nahi hai bada chutiya banaya tumko")    
+
+
+# name ="pankaj"
+# age = 17
+
+# if name == "pankaj":
+#     print("You are about to visit")
+#     if age >= 18:
+#         print("You can visit")
+#     else:
+#         print("Name is correct but age is not right")    
+# else:
+#     print("No visit")        
+
+
+
+
+a= 10
+b=100
+
+if b > a:
+    pass        # agar muje ye khali rakhna hua to pass likh sakta hu 
