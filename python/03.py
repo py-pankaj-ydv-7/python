f29484dfb2d6246f46b2007acd6addce473a378f
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:00:12 +0530

    Add files via upload
    learning OPERATOR

---
+++ python/03.py
@@ -0,0 +1,61 @@
+# operator
+
+# Arithmetic operator
+
+x = 10
+y = 5
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y) 
+# print(x ** y) #  power k liye 
+# print(x // y) #  floor division hota hai 
+
+
+
+# assignment operator 
+
+# x = 5
+# x+=3 # x = x+3;
+# print(x)
+
+
+# Comparision Operator
+
+x = 5
+y = 3
+
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+
+
+# logical operator 
+
+# 3 type k logical operator hote hai 
+
+# or || not || and
+
+# and operator 
+
+# print(5>2 and 2>5)
+# print(1>2 and 2>5)
+# print(1>2 and 9>5)
+# print(4>2 and 9>5)
+
+# or operator 
+
+# print(5>2 or 2>5)
+# print(1>2 or 2>5)
+# print(1>2 or 9>5)
+# print(4>2 or 9>5)
+
+
+
+# not operator
+print(not(False))
+print(not(True))
