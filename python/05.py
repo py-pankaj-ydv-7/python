43444e00efc30634d9b77f37cb218f1a7f8aca87
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:03:25 +0530

    Add files via upload
    string

---
+++ python/05.py
@@ -0,0 +1,34 @@
+# Strings 
+
+a = "Hello  World"
+# print(a)
+# print(len(a)) # for get length of string 
+
+# print("Hello "in a )
+# print("Hello "not in a )
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())  # space hat jata hai 
+
+# print(a.replace('H','J'))
+# print(a.split(" "))  # do chije alag alag ho jati hai 
+
+
+# format ka use 
+
+a = "He {} llo {}"
+
+b = 100
+
+c= 400
+
+# print(a.format(c,b))
+
+# agar hum chahte hai ki hamara Hello World  = Hello "World"  is tarike se  ayee to 
+
+# print("Hello \"World\"") # ese karna padega 
+print("pankaj \"Yadav\"")
+#new line 
+
+# print("Hello \nworld")
+
