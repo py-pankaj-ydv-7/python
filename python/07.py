61e61d20812e28f499e1b3395a84f82d3ff8827c
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-10 21:07:07 +0530

    Add files via upload
    list

---
+++ python/07.py
@@ -0,0 +1,27 @@
+# list creat 
+
+#  list kya hota hai :-  isme hum multiple item store karte hai 
+
+# student1 = "pankaj"
+# student2 = "Shubham"  # bahut lendhi hai 
+# student3 = "Dheeraj"
+
+# python me 4 kuch ese data type hai jo store karte hai collection of data ko : list , tuple, set,  dictionories hoti hai
+students = ["pankaj","shubham","swati"] # 0 index se suru hote hai , or ye changable hai add , remove , wagera kar sakte hai 
+# print(students)                        #  list containe different different data types
+print(len(students))
+
+print(type(students))
+
+
+thisList = list(("pankaj","shubham","swati"))
+print(thisList[1]) # yaha list ko array ki tarh hi access kiya jata hai but isem -1 , -2 se no. ya list ke element piche se print ya access kiye jate hai .
+print(thisList[0])
+print(thisList[-1])
+print(thisList[0:3]) # range bhi print karate hai 
+print(thisList[0:2]) # range bhi print karate hai 
+
+
+
+
+#  iske method documentation se pad lena hai 
