96d09ba006828eb43c36f48706bd7a2e164e0bed
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-11 15:28:10 +0530

    Add files via upload
    Recursion

---
+++ python/15.py
@@ -0,0 +1,26 @@
+#  Recursion :- function k under function call karna .
+
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:                                   # factorial 
+#         return n * factorial(n-1)
+
+# factorial = factorial(5)
+# print(factorial)
+
+
+
+
+#   lamda function kya hota hai small annonumus function hota hai ., isme number arguements likhte hai or isme ek hi expression hota hai
+
+# lambda argument : expresion
+
+#  ye single statement k liye hota hai 
+
+x = lambda a: a + 10
+
+print(x(5))
+
+
