05c04e044982bb86e11960f74780f1aa65e50d61
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-11 15:27:14 +0530

    Add files via upload
    for loop

---
+++ python/13.py
@@ -0,0 +1,52 @@
+
+#  for loop
+
+# fruits = ["Apple","Banana","Cherry"]
+# for fruit in fruits:
+#     print(fruit)
+#     # print(type(fruit))
+
+
+
+# s = "Hello"
+
+# for x in s:
+#     print(x)
+
+
+# for x in range(6):
+#     print(x)
+
+
+# l1 = [0,1,2,3,4]
+
+# for x in l1:
+#     pass
+
+
+# thistuple = ("apple","banana","orange")
+# for x in thistuple:
+#     print(x )
+
+# i = 0
+# thistuple = ("apple","banana","orange")
+# for x in range(len(thistuple)):
+#     print(thistuple[i])
+#     i+=1
+
+
+
+name= {
+    "fname": "pankaj",
+    "lname": "yadav"
+}
+
+# for n in name: # keys print karne k liye 
+#     print(n)
+
+# for n in name:
+#  print(name[n])
+
+
+for x,y in name.items():
+    print(x , y)
