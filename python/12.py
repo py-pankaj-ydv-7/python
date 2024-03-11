923812d4a2f076a2e4f443eb679927db7dd2bd55
Pankaj Yadav <89732509+py-pankaj-ydv-7@users.noreply.github.com>
2024-03-11 15:26:45 +0530

    Add files via upload
    while loop

---
+++ python/12.py
@@ -0,0 +1,37 @@
+print("Hello World")
+
+# while(True):
+#     print("hello world") self try
+
+i = 0
+
+# while i<=10 :
+#     print(i," Hello World")
+#     i+=1
+
+
+
+# while i<=10 :
+#     print(i," Hello World")
+#     if i ==4:
+#         break
+#     i+=1
+
+
+
+# while i<=10 :
+#     # print(i," Hello World")
+#     if i ==4:
+#         continue
+#     print (i)
+#     i+=1
+
+
+# while i<10 :
+#     i+=1
+#     print(f"2 * {i} = {2*i}")
+
+
+
+
+
